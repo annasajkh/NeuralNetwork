fucking fix softmax function

libs/activation_functions.py
@@ -8,15 +8,29 @@ class ActivationFunction:
         self.id = id
 
 
-def softmax(x):
-    f = np.exp(x - np.max(x))
+def softmax(arr):
+    f = np.exp(arr - np.max(arr))
     return f / np.sum(f)
+
+def d_softmax(arr):
+    jacobian_m = np.array([np.diag(arr)])
+
+
+    for i in range(len(jacobian_m)):
+        for j in range(len(jacobian_m)):
+            if i == j:
+                jacobian_m[i][j] = arr[i] * (1-arr[i])
+            else:
+                jacobian_m[i][j] = -arr[i]*arr[j]
+
+    return jacobian_m
+
 
 sigmoid = ActivationFunction(lambda arr : np.exp(np.fmin(arr, 0)) / (1 + np.exp(-np.abs(arr))), lambda arr : arr * (1 - arr), 0)
 leaky_relu = ActivationFunction(lambda arr : ((arr > 0) * arr) + ((arr <= 0) * arr * 0.01), lambda arr : ((arr > 0) * 1) + ((arr <= 0) * 0.01), 1)
 tanh = ActivationFunction(lambda arr: np.tanh(arr), lambda arr : 1 - arr ** 2, 2)
 relu = ActivationFunction(lambda arr : (arr > 0) * arr, lambda arr : (arr > 0) * 1, 3)
-softmax = ActivationFunction(softmax, None, 4)
+softmax = ActivationFunction(softmax, d_softmax, 4)
 
 def get_function(id: int) -> ActivationFunction:
     if id == sigmoid.id:

libs/layer_dense.py
@@ -21,11 +21,7 @@ class LayerDense:
     
     def step(self, errors: np.ndarray, layer : ndarray, after_layer : np.ndarray, learning_rate : float) -> None:
         """change weights and biases by errors"""
-
-        if self.activation_function_id  != softmax.id:
-            gradient : ndarray = self.activation_function.derivative(layer) * errors * learning_rate
-        else:
-            gradient : ndarray = (layer - errors) * learning_rate
+        gradient : ndarray = self.activation_function.derivative(layer) * errors * learning_rate
 
         delta_weights : np.ndarray = np.dot(gradient, after_layer.T)
 